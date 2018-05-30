70508b7f3f279a4a3f931f217d2f8840889ee454
dan <danbrianlewis@gmail.com>
2018-05-30 11:26:42 -0400

    pre-revamp commit

--- app/data/friend.js
+++ app/data/friend.js
@@ -9,7 +9,16 @@ let users = [
     photo: "https://i.imgur.com/LbDUJDk.jpg",
     preferences: [5,5,2,4,5,2,2,2,1,3]
   },
-
+  {
+    nameUser: "Stephen Barrone",
+    photo: "http://i0.kym-cdn.com/photos/images/original/000/957/777/4e9.jpg",
+    preferences: [1,2,2,4,2,2,2,1,2,5]
+  },
+  {
+    nameUser: "Perry Marrow",
+    photo: "https://adobe99u.files.wordpress.com/2018/01/antonio-guillem-girl-winning-good-news-stock-photography.jpg?quality=100&w=1640&h=1200",
+    preferences: [1,2,1,1,2,2,5,1,2,1]
+  }
 ];
 
 module.exports = users;
